646b3029c0586545b7b3b5f9d72b1f8a86d90b72
Messiovo <messiovo@gmail.com>
2022-10-31 07:31:34 +0100

    create social icon props

--- src/Component/SocialIcon.jsx
+++ src/Component/SocialIcon.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Git, Slack } from "../assets/icons";
 
 const SocialIcon = () => {
   return (
-    <div>SocialIcon</div>
-  )
-}
+    <div className="flex gap-3">
+      <Slack className="w-6 h-6" />
+      <a href="https://github.com/Mercy82104082">
+        <Git className="w-6 h-6" />
+      </a>
+    </div>
+  );
+};
 
-export default SocialIcon
+export default SocialIcon;
