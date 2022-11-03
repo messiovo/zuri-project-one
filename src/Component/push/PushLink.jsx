63358f76ed910ea6ea26e0e50c24186dec9045fc
Messiovo <messiovo@gmail.com>
2022-11-03 19:14:47 +0100

    style: update on styling

--- src/Component/push/PushLink.jsx
+++ src/Component/push/PushLink.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 const PushLink = ({ to, id, text }) => {
   return (
-    <Link to={to} id={id} className="block bg-primary px-6 py-4 lg:w-3/4 w-full text-center rounded-lg font-medium cursor-pointer hover:bg-gray-400 transition-colors ease-in-out duration-700">
+    <Link to={to} id={id} className="block bg-primary px-6 py-4 lg:w-3/4 w-full text-center rounded-lg font-medium cursor-pointer hover:bg-gray-300 transition-colors ease-in-out duration-700">
       {text}
     </Link>
   );
