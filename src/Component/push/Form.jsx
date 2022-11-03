57d1cb321408296f786981792dfb59ff5f013e1d
Messiovo <messiovo@gmail.com>
2022-11-03 19:07:09 +0100

    style: added button and style

--- src/Component/push/Form.jsx
+++ src/Component/push/Form.jsx
@@ -7,6 +7,7 @@ const Form = () => {
         <span className="flex flex-col lg:w-2/4 w-full">
           <label
             htmlFor="first name"
+            id="first_name"
             className=" text-sm text-medium font-medium"
           >
             First name
@@ -20,6 +21,7 @@ const Form = () => {
         <span className="flex flex-col lg:w-2/4 w-full">
           <label
             htmlFor="last name"
+            id="last_name"
             className=" text-sm font-medium text-medium"
           >
             Last name
@@ -34,6 +36,7 @@ const Form = () => {
       <div className="flex flex-col w-full mb-4">
         <label
           htmlFor="Email"
+          id="email"
           className=" text-sm font-medium text-medium"
         >
           Email
@@ -44,21 +47,33 @@ const Form = () => {
           placeholder="yourname@email.com"
         />
       </div>
-      <div className="flex flex-col w-full ">
-        <label
-          htmlFor="Message"
-          className="text-sm text-medium font-medium"
-        >
+      <div className="flex flex-col w-full mb-4">
+        <label htmlFor="Message" className="text-sm text-medium font-medium">
           Message
         </label>
         <textarea
-          name=""
-          id=""
+          id="message"
           rows="4"
           className="outline-none  text-base py-4 font-normal px-4 border-[1px] shadow-sm rounded-lg"
           placeholder="Send me a message and I'll reply you as soon as possible... "
         ></textarea>
       </div>
+      <div className="flex gap-2">
+        <input
+          type="checkbox"
+          className="required:border-[#D0D5DD] required:rounded-xl"
+        />
+        <label htmlFor="" className="text-base text-medium">
+          You agree to providing your data to who may contact you.
+        </label>
+      </div>
+
+      <button
+        id="btn__submit"
+        className="border-[1px] w-full mt-4 py-2 bg-[#1570EF] text-white rounded-lg shadow-sm"
+      >
+        Send message
+      </button>
     </form>
   );
 };
--- src/Page/Contact.jsx
+++ src/Page/Contact.jsx
@@ -3,7 +3,7 @@ import {Form} from '../Component/push'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='pb-20'>
         <h1 className='text-4xl font-bold mb-4'>Contact Me</h1>
         <p className='text-dark mb-8 text-lg lg:text-xl'>Hi there, contact me to ask me about anything you have in mind.</p>
         <Form/>
